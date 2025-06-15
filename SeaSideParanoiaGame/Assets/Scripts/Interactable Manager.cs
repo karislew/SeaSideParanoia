0d5ed4a0c8544ef3a534fd5a82e008bb7ac02a9d
karislew <157058967+karislew@users.noreply.github.com>
2025-06-15 00:15:13 -0500

    Mini Interaction System
    able to click the objects in the scene (if interactable) and call their associated function

--- SeaSideParanoiaGame/Assets/Scripts/Interactable Manager.cs
+++ SeaSideParanoiaGame/Assets/Scripts/Interactable Manager.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using GHEvtSystem;
 using System.Drawing.Printing;
+using Unity.VisualScripting;
+using System;
 
-
-public class InteractableManager : MonoBehaviour
+public class MouseInteract : GHEvtSystem.Event
+    {
+        public Vector3 position;
+    }
+public class InteractableManager 
 {
     private static InteractableManager instance;
+    private Interactable closestInteractable = null;
     private InteractableManager()
     {
         EventDispatcher.Instance.AddListener<MouseInteract>(Testing);
     }
+    private List<Interactable> interactables = new List<Interactable>();
     static public InteractableManager GetInstance()
     {
         if (instance == null)
@@ -20,10 +27,44 @@ public class InteractableManager : MonoBehaviour
         }
         return instance;
     }
+    public void AddInteractable(Interactable interactable_obj)
+    { 
+        interactables.Add(interactable_obj);
+    }
+    public void RemoveInteractable(Interactable interactable_obj)
+    { 
+        interactables.Remove(interactable_obj);
+    }
     public void Testing(MouseInteract evt)
     {
-        print("WORK PLEASE");
-        Debug.Log("Is this thing working");
+
+        GettingInteractable(evt.position);
+    }
+    public void GettingInteractable(Vector3 position)
+    {
+        
+        float maxDistance = Mathf.Infinity;
+        closestInteractable = null;
+       //checking each interactable obj in the interactables list 
+        foreach (Interactable interactable in interactables)
+        {
+            //getting the distance between the interactable objects and the mouseposition 
+          
+            float distance = Vector3.Distance(position, interactable.transform.position);
+            //if its lower than the maxdistance
+            //set the new distance and make that object associated the closest Interactable
+            if (distance < maxDistance)
+            {
+                maxDistance = distance;
+                closestInteractable = interactable;
+            }
+        }
+        //call that interactable objects OnInteract function
+        if (closestInteractable != null)
+        {
+            closestInteractable.OnInteract();
+        }
+        return;
     }
 }
 
--- SeaSideParanoiaGame/Assets/Scripts/MouseInteraction.cs
+++ SeaSideParanoiaGame/Assets/Scripts/MouseInteraction.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using GHEvtSystem;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -9,43 +10,72 @@ using UnityEngine.EventSystems;
 
 public class MouseInputSystem : MonoBehaviour
 {
+    [SerializeField] private Camera mainCamera;
+    private Vector3 mousePosition;
+    [SerializeField] private LayerMask layerMask;
    
+
     public void OnMouseDown()
     {
-        MouseInteract mouseInteract = new MouseInteract()
-        {
-            MouseClicked = true
-        };
-        EventDispatcher.Instance.RaiseEvent<MouseInteract>(mouseInteract);
-        print($"we down fr {this.name}");
+        //print($"we down fr {this.name}");
+        //MouseInteract mouseInteract = new MouseInteract()
+        
+        //EventDispatcher.Instance.RaiseEvent<MouseInteract>(mouseInteract);
+
     }
     public void OnMouseEnter()
     {
        
+       
     }
     public void OnMouseExit()
     {
-        
+       
         
     }
 
 
-    // Start is called before the first frame update
-    void Start()
-   {
 
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Realized that if i did OnMouseDown this script would need to be on each interactable object
+        //so now im checking if the mouse if down, doing physics raycast and if it hits and interactable (layermask), 
+        //since I cant click on 2D objects with the 3D raycast I added another 2D raycast to check if im clicking 
+        //a 2d object. 
+        //if clicked sending that mouse position and raising the event 
+        if (Input.GetMouseButtonDown(0))
+        {
+            //if 3d object
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit raycastHit, Mathf.Infinity, layerMask))
+            {
+                mousePosition = raycastHit.point;
+
+            }
+            //if 2d object
+            else
+            {
+                Vector3 mouse2DPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+                RaycastHit2D raycastHit2D = Physics2D.Raycast(mouse2DPosition, Vector2.zero, Mathf.Infinity, layerMask);
+                if (raycastHit2D.collider != null)
+                {
+                    mousePosition = raycastHit2D.point;
+
+                }
+
+            }
+            //raising event
+             MouseInteract mouseInteract = new MouseInteract()
+            {
+                position = mousePosition
+            };
+            EventDispatcher.Instance.RaiseEvent<MouseInteract>(mouseInteract);
+
+        }
    }
 
-
-   // Update is called once per frame
-   void Update()
-   {
-
-
-   }
-
-
- 
 }
 
 
