0c79ec5ce291dbd02024fe7810bef64584d8fb05
Melissa Gray <61853436+mag535@users.noreply.github.com>
2025-07-24 20:44:59 -0400

    Moved scripts into script folder

---
+++ SeaSideParanoiaGame/Assets/Scripts/DragItem.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DragItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Image image;
+    public Transform parentAfterDrag;
+    public Clue clue;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("begin drag");
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+        image.raycastTarget = false;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Dragging");
+        transform.position = Input.mousePosition;
+        
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("end drag");
+        transform.SetParent(parentAfterDrag);
+        image.raycastTarget = true;
+
+    }
+}
